9c22d897e86f09a2b8b5aa9d149c41f80c8d0c1f
zqz2020 <60119645+zqz2020@users.noreply.github.com>
2020-04-13 12:41:27 -0600

    finish cleaning

--- src/front/DocReadPat.java
+++ src/front/DocReadPat.java
@@ -36,6 +36,8 @@ public class DocReadPat extends JPanel {
 		this.p = p;
 		this.user = user;
 		
+		setLayout(null);
+		
 		JLabel lblNewLabel = new JLabel("Patient information:");
 		heightWeight = new JLabel();
 		heightWeight.setText("hw");
@@ -57,57 +59,7 @@ public class DocReadPat extends JPanel {
 				Main.window.setScreen(Main.Screen.PLIS, user, null);
 			}
 		});
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(177, Short.MAX_VALUE)
-					.addComponent(btnNewButton)
-					.addGap(218))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(160, Short.MAX_VALUE)
-					.addComponent(name)
-					.addGap(201))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(157)
-					.addComponent(lblNewLabel)
-					.addContainerGap(198, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGap(144)
-					.addComponent(heightWeight, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(181, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(birth, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE))
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-							.addGap(173)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(sex, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cond))))
-					.addContainerGap(150, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel)
-					.addGap(29)
-					.addComponent(heightWeight)
-					.addGap(18)
-					.addComponent(name)
-					.addGap(49)
-					.addComponent(cond)
-					.addGap(34)
-					.addComponent(sex)
-					.addGap(29)
-					.addComponent(birth)
-					.addGap(12)
-					.addComponent(btnNewButton)
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		setLayout(groupLayout);
+		
 		
 	}
 	
